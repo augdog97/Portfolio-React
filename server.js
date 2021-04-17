04bc9d6a0f409e27608585369855bfe21b606dd4
August Shah <augustshah@02pilot.com>
2021-04-17 16:08:51 -0400

    Stable: Removed secret

--- server.js
+++ server.js
@@ -4,8 +4,24 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  
+ const stripe = require('stripe')(`${process.env.REACT_APP_Stripe_Live}`);
+ const invoice = await stripe.invoices.retrieve(
+   ''
+ );
+
+  console.log(JSON.stringify(invoice));
+
 });
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 app.listen(process.env.PORT || 8080);
